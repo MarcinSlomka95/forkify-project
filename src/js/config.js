c5089ad79d782d9316bc811c20f5c3ed74893120
MarcinSlomka95 <marcin.slomka95@interia.pl>
2024-08-14 15:01:41 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '41b392c2-6fb2-400f-92f7-5d2eb0ac8594';
+export const MODAL_CLOSE_SEC = 2.5;
